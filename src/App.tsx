e092fe820c690f3b0e24aed5a69584a02e9b8a67
ragu <raguct25@gmail.com>
2022-07-15 12:03:38 +0530

    code standard configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <header>
+        <h1>React Project</h1>
         <img src={logo} alt="logo" style={{ width: 250, height: 250 }} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
