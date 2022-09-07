a86fedc2fba6e5f9ca0b45c1f0d8eea1f1f90618
Gibson <gibsonjoseph890@gmail.com>
2022-09-07 19:17:29 +0530

    folder changed

--- src/containers/layout/Dashboard.jsx
+++ src/containers/layout/Dashboard.jsx
@@ -2,7 +2,7 @@
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import { Outlet } from 'react-router-dom';
-import requireAuth from './Hoc';
+import requireAuth from '../../hoc/Hoc';
 import WelcomeBanner from '../components/welcomebaner';
 
 import React, { useState } from 'react';
